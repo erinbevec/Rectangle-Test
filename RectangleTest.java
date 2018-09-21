666527449eb75d1aa94852f2af7cfedc59226311
erinbevec <42977057+erinbevec@users.noreply.github.com>
2018-09-21 01:09:39 -0400

    Add files via upload

---
+++ RectangleTest.java
@@ -0,0 +1,31 @@
+import java.awt.Rectangle;
+
+public class RectangleTest {
+
+	public static void main(String[] args)
+	{
+		//look in Rectangle API under constructors to find what Rectangle(5,10,20,30) does
+		Rectangle box = new Rectangle(5, 10, 20, 30);
+		public void Rectangle setLocation(1,6);
+		int xcoordinate = double Rectangle getX();
+		int ycoordinate = double Rectangle getY();
+		System.out.println("The predicted x-coordinate is 1");
+		System.out.println("The actual x-coordinate is " + xcoordinate);
+		System.out.println("The new y-coordinate is 6");
+		System.out.println("The actual y-coordinate is " + ycoordinate);
+		
+		
+		//TODO Move the rectangle (use a method from the API to move the rectangle
+		//                          by changing both the x and y coordinates)
+		
+		//TODO Print the following information in the following format
+		/*
+		 * Expected new x value: ... (print what you expect the x value to be)
+		 * Actual x value: ...  (use a method from the API to print the actual x value)
+		 * Expected new y value: ... (print what you expect the new y value to be)
+		 * Actual y value: ...  (use a method from the API to print the actual y value)
+		 */
+
+	}
+
+}
